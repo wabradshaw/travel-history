c728314a0de576268cafac35cbcf59c8dc87ae35
William Bradshaw <w.a.bradshaw@gmail.com>
2018-08-02 13:56:07 +0200

    Added the table definition for the main history table.

---
+++ src/main/kotlin/com/wabradshaw/travelhistory/Tables.kt
@@ -0,0 +1,20 @@
+package com.wabradshaw.travelhistory
+
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import org.joda.time.DateTime
+
+/**
+ * The definition of the main History table which contains all of the details about each historical event.
+ */
+object History : Table() {
+    val uuid: Column<Int> = integer("uuid").primaryKey().autoIncrement()
+    val startTime: Column<DateTime> = datetime("start_time")
+    val endTime: Column<DateTime?> = datetime("end_time").nullable()
+    val name: Column<String> = varchar("name", length=600)
+    val country: Column<String> = varchar("country", length=600)
+    val timezone: Column<Int> = integer("timezone_offset")
+    val blogPostUrl: Column<String?> = varchar("blog_post_url", length=6000).nullable()
+    val blogPostName: Column<String?> = varchar("blog_post_name", length=6000).nullable()
+    val mapUrl: Column<String?> = varchar("map_url", length=6000).nullable()
+}
